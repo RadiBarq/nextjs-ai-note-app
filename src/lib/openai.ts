87f97a16f96b3fc158bdb8731b2037a945a36e8b
radibaraq <radibaraq@cloud.upwork.com>
2024-03-10 14:38:04 +0200

    open ai and pinecone setup

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,5 +1,12 @@
+import OpenAI from "openai";
+
 const apiKey = process.env.OPENAI_API_KEY;
 
 if (!apiKey) {
   throw Error("OPENAI_API_KEY is not set");
 }
+
+const openai = new OpenAI({ apiKey });
+
+// Then we export the default open ai
+export default openai;
