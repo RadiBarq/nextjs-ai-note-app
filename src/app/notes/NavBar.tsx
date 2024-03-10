3e79af72ead2773eb0020134167ed004f242b549
radibaraq <radibaraq@cloud.upwork.com>
2024-03-10 14:28:12 +0200

    Setup theme and updating and deleting notes

--- src/app/notes/NavBar.tsx
+++ src/app/notes/NavBar.tsx
@@ -1,32 +1,49 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/assets/logo.png";
 import { UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
+import { useState } from "react";
+import { dark } from "@clerk/themes";
+import AddNoteDialog from "@/components/AddEditNoteDialog";
+import ThemeToggleButton from "@/components/ThemeToggleButton";
+import { useTheme } from "next-themes";
 
 export default function NavBar() {
+  const { theme } = useTheme();
+  const [showAddEditNoteDialog, setShowAddEditNoteDialog] = useState(false);
   return (
-    <div className="p-4 shadow">
-      <div className="m-auto max-w-7xl flex flex-wrap gap-3 items-center justify-between">
-        <Link href="/notes" className="flex items-center gap-1">
-          <Image src={logo} alt="FlowBrain logo" width={40} height={40} />
-          <span className="font-bold">KindiAI</span>
-        </Link>
-        <div className="flex items-center gap-2">
-          <UserButton
-            afterSignOutUrl="/"
-            appearance={{
-              elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
-            }}
-          />
-
-          <Button>
-            <Plus size={20} className="mr-2" />
-            Add note
-          </Button>
+    <>
+      <div className="p-4 shadow">
+        <div className="m-auto max-w-7xl flex flex-wrap gap-3 items-center justify-between">
+          <Link href="/notes" className="flex items-center gap-1">
+            <Image src={logo} alt="FlowBrain logo" width={40} height={40} />
+            <span className="font-bold">KindiAI</span>
+          </Link>
+          <div className="flex items-center gap-2">
+            <UserButton
+              afterSignOutUrl="/"
+              appearance={{
+                baseTheme: theme === "dark" ? dark : undefined,
+                elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
+              }}
+            />
+            <ThemeToggleButton />
+            <Button onClick={() => setShowAddEditNoteDialog(true)}>
+              <Plus size={20} className="mr-2" />
+              Add note
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+
+      <AddNoteDialog
+        open={showAddEditNoteDialog}
+        setOpen={setShowAddEditNoteDialog}
+      />
+    </>
   );
 }
--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,9 +1,30 @@
+import { auth } from "@clerk/nextjs";
 import { Metadata } from "next";
+import prisma from "@/lib/db/prisma";
+import Note from "@/components/Note";
 
 export const metadata: Metadata = {
   title: "FlowBrain - Notes",
 };
 
-export default function NotesPage() {
-  return <div>Here will be your notes</div>;
+export default async function NotesPage() {
+  const { userId } = auth();
+
+  if (!userId) throw Error("userId undefined");
+
+  const allNotes = await prisma.note.findMany({ where: { userId } });
+
+  return (
+    <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3">
+      {allNotes.map((note) => (
+        <Note note={note} key={note.id} />
+      ))}
+      {allNotes.length === 0 && (
+        // We can escape this signle quotes or add them inside curly braces.
+        <div className="col-span-full text-center">
+          {"You don't have any notes yet. Why don't you create one?"}
+        </div>
+      )}
+    </div>
+  );
 }
